c209a753c54429a21ea9e4e5ac5628dc633063b0
Syu fu <syu.fu.dev@gmail.com>
2026-02-14 18:05:52 +0900

    refactor: use new git settings options (non-deprecated)

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -2,15 +2,16 @@
   programs.git = {
     enable = true;
 
-    userName = "Syu-fu";
-    userEmail = "syu-fu@gmail.com";
-
     signing = {
       key = "1D6505310110B5B8BD33D812ABC41001BC030833";
       signByDefault = true;
     };
 
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Syu-fu";
+        email = "syu-fu@gmail.com";
+      };
       init.defaultBranch = "main";
       pull.rebase = false;
       core.editor = "nvim";
