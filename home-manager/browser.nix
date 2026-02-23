0f7abde94427c5ad782299093a7479c7cf393edb
Syu fu <syu.fu.dev@gmail.com>
2026-02-23 16:03:55 +0900

    feat: add Toggl Track extension for Firefox

--- home-manager/browser.nix
+++ home-manager/browser.nix
@@ -5,12 +5,17 @@ let
     url = "https://addons.mozilla.org/firefox/downloads/file/4618554/vimium_ff-2.3.1.xpi";
     hash = "sha256-0wqlb4iik74h1jilkif20zl6br3l3rfvjq2fdsic4f8rnhf8c6rc=";
   };
+  toggl = pkgs.fetchFirefoxAddon {
+    name = "toggl";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4666536/toggl_button_time_tracker-4.11.8.xpi";
+    hash = "sha256-11pw6q3fqv4yf8ma59izn0qfmhyhbhh1fcpbgvijfpn7p2k336nd=";
+  };
 in
 {
   programs.firefox = {
     enable = true;
     profiles.default = {
-      extensions.packages = [ vimium ];
+      extensions.packages = [ vimium toggl ];
     };
   };
 
