1090555304e293998377bb89d8bc69cb0b9e50d7
Syu fu <syu.fu.dev@gmail.com>
2026-02-23 16:09:55 +0900

    feat: add 1Password extension for Firefox

--- home-manager/browser.nix
+++ home-manager/browser.nix
@@ -10,12 +10,17 @@ let
     url = "https://addons.mozilla.org/firefox/downloads/file/4666536/toggl_button_time_tracker-4.11.8.xpi";
     hash = "sha256-11pw6q3fqv4yf8ma59izn0qfmhyhbhh1fcpbgvijfpn7p2k336nd=";
   };
+  onepassword = pkgs.fetchFirefoxAddon {
+    name = "1password";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4692796/1password_x_password_manager-8.12.2.38.xpi";
+    hash = "sha256-1ph43d7az1wgcbzzs9fzvkyzc275hgj0m6c8r3sbsfm2g8612b95=";
+  };
 in
 {
   programs.firefox = {
     enable = true;
     profiles.default = {
-      extensions.packages = [ vimium toggl ];
+      extensions.packages = [ vimium toggl onepassword ];
     };
   };
 
