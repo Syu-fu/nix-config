d8ef03d03e01d947b7d2e3ec2e2d624aa4698e52
Syu fu <syu.fu.dev@gmail.com>
2026-03-28 15:51:13 +0900

    fix(hyprpanel): use home.activation to patch icon instead of clobbering config.json

--- home-manager/hyprpanel.nix
+++ home-manager/hyprpanel.nix
@@ -1,11 +1,15 @@
 { lib, pkgs, ... }:
 lib.mkIf pkgs.stdenv.isLinux {
-  xdg.configFile."hyprpanel/config.json".text = builtins.toJSON {
-    bar = {
-      launcher = {
-        icon = "";
-        autoDetectIcon = false;
-      };
-    };
-  };
+  home.activation.hyprpanelConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    config_dir="$HOME/.config/hyprpanel"
+    config_file="$config_dir/config.json"
+    mkdir -p "$config_dir"
+    if [ ! -f "$config_file" ]; then
+      echo '{}' > "$config_file"
+    fi
+    tmp=$(${pkgs.jq}/bin/jq \
+      '.bar.launcher.icon = "" | .bar.launcher.autoDetectIcon = false' \
+      "$config_file")
+    echo "$tmp" > "$config_file"
+  '';
 }
