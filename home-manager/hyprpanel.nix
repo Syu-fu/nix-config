2b05d601feda1291149e3a422b6e363a6309e3ce
Syu fu <syu.fu.dev@gmail.com>
2026-03-28 16:18:48 +0900

    fix(hyprpanel): always set layouts with dashboard on left, set scalingPriority to hyprland

--- home-manager/hyprpanel.nix
+++ home-manager/hyprpanel.nix
@@ -8,15 +8,24 @@ lib.mkIf pkgs.stdenv.isLinux {
       printf '{}' > "$config_file"
     fi
     nix_icon=$(printf '\xef\x8c\x93')
+    default_layouts='{"0":{"left":["dashboard","workspaces","windowtitle"],"middle":["media"],"right":["volume","network","bluetooth","battery","systray","clock","notifications"]},"1":{"left":["dashboard","workspaces","windowtitle"],"middle":["media"],"right":["volume","clock","notifications"]},"2":{"left":["dashboard","workspaces","windowtitle"],"middle":["media"],"right":["volume","clock","notifications"]}}'
     tmp=$(mktemp)
-    if ${pkgs.jq}/bin/jq --arg icon "$nix_icon" \
-      '.bar.launcher.icon = $icon
+    if ${pkgs.jq}/bin/jq --arg icon "$nix_icon" --argjson defaults "$default_layouts" \
+      '. as $cur
+        | .bar.launcher.icon = $icon
         | .bar.launcher.autoDetectIcon = false
-        | if .bar.layouts then
-            .bar.layouts |= with_entries(
-              .value.left = (["dashboard"] + (.value.left // [] | map(select(. != "dashboard"))))
+        | .scalingPriority = "hyprland"
+        | .bar.layouts = (
+            $defaults | with_entries(
+              .key as $k |
+              .value.left = (
+                ["dashboard"] + (
+                  ($cur.bar.layouts[$k].left // .value.left)
+                  | map(select(. != "dashboard"))
+                )
+              )
             )
-          else . end' \
+          )' \
       "$config_file" > "$tmp"; then
       mv "$tmp" "$config_file"
     else
