9c2979398aaf3741a29afb29b7323c4645048f7f
Syu fu <syu.fu.dev@gmail.com>
2026-03-28 16:35:53 +0900

    fix(hyprpanel): use numeric monitor keys instead of wildcard for layouts

--- home-manager/hyprpanel.nix
+++ home-manager/hyprpanel.nix
@@ -11,10 +11,20 @@ lib.mkIf pkgs.stdenv.isLinux {
           autoDetectIcon = false;
         };
         layouts = {
-          "*" = {
+          "0" = {
             left = [ "dashboard" "workspaces" "windowtitle" ];
             middle = [ "media" ];
             right = [ "volume" "network" "bluetooth" "battery" "systray" "clock" "notifications" ];
+          };
+          "1" = {
+            left = [ "dashboard" "workspaces" "windowtitle" ];
+            middle = [ "media" ];
+            right = [ "volume" "clock" "notifications" ];
+          };
+          "2" = {
+            left = [ "dashboard" "workspaces" "windowtitle" ];
+            middle = [ "media" ];
+            right = [ "volume" "clock" "notifications" ];
           };
         };
       };
