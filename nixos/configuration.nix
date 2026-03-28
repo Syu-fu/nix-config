7be2dbeab4e28e7e4b0a28869ae4f1b9d34ef572
Syu fu <syu.fu.dev@gmail.com>
2026-03-28 18:55:59 +0900

    fix(nixos): use start-hyprland in greetd to suppress watchdog warning

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,7 +58,7 @@
     greetd = {
       enable = true;
       settings.default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd start-hyprland";
         user = "greeter";
       };
     };
