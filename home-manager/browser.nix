c40899191378031bfe71b0eb147a1bc65571fe1a
Syu-fu <syu-fu@gmail.com>
2026-04-05 15:22:03 +0900

    fix(firefox): use policies for extension install and set default browser

--- home-manager/browser.nix
+++ home-manager/browser.nix
@@ -1,36 +1,40 @@
 { pkgs, ... }:
-let
-  vimium = pkgs.fetchFirefoxAddon {
-    name = "vimium";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4618554/vimium_ff-2.3.1.xpi";
-    hash = "sha256-LBuGHLQZOcKibk5guV0edORl6AfCxUmjDJCcGSNZFHM=";
-  };
-  toggl = pkgs.fetchFirefoxAddon {
-    name = "toggl";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4666536/toggl_button_time_tracker-4.11.8.xpi";
-    hash = "sha256-zZoxprjHXifjfusyFyBc0MPqMLA/pqIqcp5s7AY2/IY=";
-  };
-  onepassword = pkgs.fetchFirefoxAddon {
-    name = "1password";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4692796/1password_x_password_manager-8.12.2.38.xpi";
-    hash = "sha256-JS0RDHqiOr30yIiZCuSD5Qj2/dzfJf3/Yo+Hr04bBN4=";
-  };
-  deepl = pkgs.fetchFirefoxAddon {
-    name = "deepl";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4682211/deepl_translate-1.72.0.xpi";
-    hash = "sha256-vxKSdOoneu/mEBU9YRnmEKIGBu1oPD7Q1j6Uvq0nHVI=";
-  };
-  obsidianWebClipper = pkgs.fetchFirefoxAddon {
-    name = "obsidian-web-clipper";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4585733/web_clipper_obsidian-0.12.0.xpi";
-    hash = "sha256-nbDIyWCaS4SJ0uF54McJob1MkbDELfvn9fXRAqHqtbc=";
-  };
-in
 {
   programs.firefox = {
     enable = true;
-    profiles.default = {
-      extensions.packages = [ vimium toggl onepassword deepl obsidianWebClipper ];
+    profiles.default = { };
+    policies = {
+      ExtensionSettings = {
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4618554/vimium_ff-2.3.1.xpi";
+          installation_mode = "force_installed";
+        };
+        "toggl-button@toggl.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4666536/toggl_button_time_tracker-4.11.8.xpi";
+          installation_mode = "force_installed";
+        };
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4692796/1password_x_password_manager-8.12.2.38.xpi";
+          installation_mode = "force_installed";
+        };
+        "firefox-extension@deepl.com" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4682211/deepl_translate-1.72.0.xpi";
+          installation_mode = "force_installed";
+        };
+        "clipper@obsidian.md" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4585733/web_clipper_obsidian-0.12.0.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
     };
   };
 
